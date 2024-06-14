db9197f147770b6a59f2ea339ea947537b0ce138
Charles Agyemang <charlesagyemang@Charless-MacBook-Pro.local>
2024-06-14 16:37:58 +0100

    my first code

---
+++ quotes.js
@@ -0,0 +1,32 @@
+var bibleQuotes = [
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+    { 
+     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
+     quoteNumber: "Mathew 6: 33"
+},
+];
+
+console.log(bibleQuotes)
