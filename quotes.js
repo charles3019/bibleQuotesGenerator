bba680e8a88116fb052ff0b51385957ff3548719
Charles Agyemang <charlesagyemang@Charless-MacBook-Pro.local>
2024-06-15 20:12:34 +0100

    arrayobject

--- quotes.js
+++ quotes.js
@@ -1,32 +1,14 @@
+// bible quotes arrayobjects 
 var bibleQuotes = [
     { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
+     bibleVerse: "But seek first the kingdom of God, and his righteousness; and all these things shall be added unto you.",
+     quoteNumber: "Matthew 6: 33"
 },
-    { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
-},
-    { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
-},
-    { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
-},
-    { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
-},
-    { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
-},
-    { 
-     bibleVerse: "But seek first the kingdom of God, and his righteousnesss; and all these things shall be added unto you."
-     quoteNumber: "Mathew 6: 33"
-},
+    // Add more unique quotes here...
 ];
+
+
+
+
 
 console.log(bibleQuotes)
